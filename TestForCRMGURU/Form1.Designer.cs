e4baf349258f88b99adab8f159d36819ce85b127
PQlavka <63120501+PQlavka@users.noreply.github.com>
2020-08-22 20:46:48 +0300

    Add files via upload

---
+++ TestForCRMGURU/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace TestForCRMGURU
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxCountry = new System.Windows.Forms.TextBox();
+            this.buttonGet = new System.Windows.Forms.Button();
+            this.buttonAllDB = new System.Windows.Forms.Button();
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.sqlConnectionMain = new System.Data.SqlClient.SqlConnection();
+            this.sqlCommandAddCountryProc = new System.Data.SqlClient.SqlCommand();
+            this.sqlSelectCommand1 = new System.Data.SqlClient.SqlCommand();
+            this.sqlDataAdapter1 = new System.Data.SqlClient.SqlDataAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 225);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(493, 196);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Страна (on english pls)";
+            // 
+            // textBoxCountry
+            // 
+            this.textBoxCountry.Location = new System.Drawing.Point(178, 10);
+            this.textBoxCountry.Name = "textBoxCountry";
+            this.textBoxCountry.Size = new System.Drawing.Size(150, 22);
+            this.textBoxCountry.TabIndex = 2;
+            // 
+            // buttonGet
+            // 
+            this.buttonGet.Location = new System.Drawing.Point(334, 10);
+            this.buttonGet.Name = "buttonGet";
+            this.buttonGet.Size = new System.Drawing.Size(113, 23);
+            this.buttonGet.TabIndex = 3;
+            this.buttonGet.Text = "Получить";
+            this.buttonGet.UseVisualStyleBackColor = true;
+            this.buttonGet.Click += new System.EventHandler(this.ButtonGet_Click);
+            // 
+            // buttonAllDB
+            // 
+            this.buttonAllDB.Location = new System.Drawing.Point(12, 196);
+            this.buttonAllDB.Name = "buttonAllDB";
+            this.buttonAllDB.Size = new System.Drawing.Size(493, 23);
+            this.buttonAllDB.TabIndex = 4;
+            this.buttonAllDB.Text = "Все страны в БД";
+            this.buttonAllDB.UseVisualStyleBackColor = true;
+            this.buttonAllDB.Click += new System.EventHandler(this.ButtonAllDB_Click);
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.ItemHeight = 16;
+            this.listBoxResult.Location = new System.Drawing.Point(16, 38);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(328, 148);
+            this.listBoxResult.TabIndex = 5;
+            // 
+            // sqlConnectionMain
+            // 
+            this.sqlConnectionMain.ConnectionString = "Data Source=DESKTOP-FCTVBII;Initial Catalog=localcsharp;Integrated Security=True";
+            this.sqlConnectionMain.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // sqlCommandAddCountryProc
+            // 
+            this.sqlCommandAddCountryProc.CommandText = "AddCountry";
+            this.sqlCommandAddCountryProc.CommandType = System.Data.CommandType.StoredProcedure;
+            this.sqlCommandAddCountryProc.Connection = this.sqlConnectionMain;
+            this.sqlCommandAddCountryProc.Parameters.AddRange(new System.Data.SqlClient.SqlParameter[] {
+            new System.Data.SqlClient.SqlParameter("@RETURN_VALUE", System.Data.SqlDbType.Int, 4, System.Data.ParameterDirection.ReturnValue, false, ((byte)(0)), ((byte)(0)), "", System.Data.DataRowVersion.Current, null),
+            new System.Data.SqlClient.SqlParameter("@name", System.Data.SqlDbType.VarChar, 50),
+            new System.Data.SqlClient.SqlParameter("@capital", System.Data.SqlDbType.VarChar, 50),
+            new System.Data.SqlClient.SqlParameter("@region", System.Data.SqlDbType.VarChar, 50),
+            new System.Data.SqlClient.SqlParameter("@code", System.Data.SqlDbType.VarChar, 50),
+            new System.Data.SqlClient.SqlParameter("@area", System.Data.SqlDbType.Real, 4),
+            new System.Data.SqlClient.SqlParameter("@pop", System.Data.SqlDbType.BigInt, 8)});
+            // 
+            // sqlSelectCommand1
+            // 
+            this.sqlSelectCommand1.CommandText = resources.GetString("sqlSelectCommand1.CommandText");
+            this.sqlSelectCommand1.Connection = this.sqlConnectionMain;
+            // 
+            // sqlDataAdapter1
+            // 
+            this.sqlDataAdapter1.SelectCommand = this.sqlSelectCommand1;
+            this.sqlDataAdapter1.TableMappings.AddRange(new System.Data.Common.DataTableMapping[] {
+            new System.Data.Common.DataTableMapping("Table", "Cities", new System.Data.Common.DataColumnMapping[] {
+                        new System.Data.Common.DataColumnMapping("Name", "Name"),
+                        new System.Data.Common.DataColumnMapping("Code", "Code"),
+                        new System.Data.Common.DataColumnMapping("Capital", "Capital"),
+                        new System.Data.Common.DataColumnMapping("Region", "Region"),
+                        new System.Data.Common.DataColumnMapping("Population", "Population"),
+                        new System.Data.Common.DataColumnMapping("Space_m2", "Space_m2")})});
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(517, 433);
+            this.Controls.Add(this.listBoxResult);
+            this.Controls.Add(this.buttonAllDB);
+            this.Controls.Add(this.buttonGet);
+            this.Controls.Add(this.textBoxCountry);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonShowAll;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxCountry;
+        private System.Windows.Forms.Button buttonGet;
+        private System.Windows.Forms.Button buttonAllDB;
+        private System.Windows.Forms.ListBox listBoxResult;
+        private System.Data.SqlClient.SqlConnection sqlConnectionMain;
+        private System.Data.SqlClient.SqlCommand sqlCommandAddCountryProc;
+        private System.Data.SqlClient.SqlCommand sqlSelectCommand1;
+        private System.Data.SqlClient.SqlDataAdapter sqlDataAdapter1;
+    }
+}
+
---
+++ TestForCRMGURU/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace TestForCRMGURU
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonGet_Click(object sender, EventArgs e)
+        {
+            buttonGet.Visible = false;
+            if (textBoxCountry.Text != "") {
+                API_CRMGURU api = new API_CRMGURU(textBoxCountry.Text); //API init
+                api.GetInfo(); //API get content JSON (api.result = json object)
+                int waitmeter = 0;
+                while (!api.result.Loaded && waitmeter <= 500) { //for async 
+                    Thread.Sleep(10);
+                    waitmeter++;
+                }
+                try
+                {
+                    listBoxResult.Items.Clear();
+                    listBoxResult.Items.Add("Название: " + api.result.MyArray[0].name);
+                    listBoxResult.Items.Add("Код страны: " + api.result.MyArray[0].alpha2Code);
+                    listBoxResult.Items.Add("Столица: " + api.result.MyArray[0].capital);
+                    listBoxResult.Items.Add("Площадь: " + api.result.MyArray[0].area + " км2");
+                    listBoxResult.Items.Add("Население: " + api.result.MyArray[0].population + " чел.");
+                    listBoxResult.Items.Add("Регион: " + api.result.MyArray[0].region);
+                    sqlCommandAddCountryProc.Parameters["@name"].Value = api.result.MyArray[0].name;
+                    sqlCommandAddCountryProc.Parameters["@capital"].Value = api.result.MyArray[0].capital;
+                    sqlCommandAddCountryProc.Parameters["@region"].Value = api.result.MyArray[0].region;
+                    sqlCommandAddCountryProc.Parameters["@code"].Value = api.result.MyArray[0].alpha2Code;
+                    sqlCommandAddCountryProc.Parameters["@area"].Value = api.result.MyArray[0].area;
+                    sqlCommandAddCountryProc.Parameters["@pop"].Value = api.result.MyArray[0].population;
+                    //Настройки подключения к MS SQL выполняются из конструктора формы в поле ConnectionString элемента sqlConnectionMain (считаю это наиболее удобным для программиста методом подключения, т.к. настройки тянутся автоматически)
+                    //sqlConnectionMain.ConnectionString = "Data Source=DESKTOP-FCTVBII;" +
+                    //                                     "Initial Catalog=localcsharp;" +
+                    //                                     "Integrated Security=True";
+
+                    sqlConnectionMain.Open();
+                    sqlCommandAddCountryProc.ExecuteNonQuery(); //AddCountry procedure
+                    sqlConnectionMain.Close();
+                }
+                catch (Exception err)
+                {
+                    listBoxResult.Items.Clear();
+                    listBoxResult.Items.Add("Err: 404 Not Founded" );
+                }
+                buttonGet.Visible = true;
+            }
+        }
+
+        private void ButtonAllDB_Click(object sender, EventArgs e)
+        {
+            dataGridView.Rows.Clear();
+            sqlConnectionMain.Open();
+            DataSet ds = new DataSet();
+            sqlDataAdapter1.Fill(ds);
+            dataGridView.DataSource = ds.Tables[0];
+            sqlConnectionMain.Close();
+        }
+    }
+}
